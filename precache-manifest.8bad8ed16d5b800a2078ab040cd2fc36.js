64676aa1843228412a1ce2188510612d1ee73643
Ayush Jain <aayushhjain23@gmail.com>
2020-11-15 18:37:51 +0530

    Add files via upload

---
+++ precache-manifest.8bad8ed16d5b800a2078ab040cd2fc36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfc52d1cc75b059bb06fc94f1c1af4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34fbeb4f333a38f4871c",
+    "url": "/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "2edb8af387891c8c1cc2",
+    "url": "/static/css/main.8a177dea.chunk.css"
+  },
+  {
+    "revision": "34fbeb4f333a38f4871c",
+    "url": "/static/js/2.e2265e5b.chunk.js"
+  },
+  {
+    "revision": "2edb8af387891c8c1cc2",
+    "url": "/static/js/main.38b05a2c.chunk.js"
+  },
+  {
+    "revision": "0d93baf50748ecf3d34c",
+    "url": "/static/js/runtime-main.8d79506e.js"
+  }
+]);
